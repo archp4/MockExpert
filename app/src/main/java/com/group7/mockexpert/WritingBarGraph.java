57663136994e52c7fe6d24c2f3d9f1f84b09fd9e
IshikaBhatia24 <bhatiaishika2402@gmail.com>
2025-04-08 23:06:20 -0400

    Writing ui added

---
+++ app/src/main/java/com/group7/mockexpert/WritingBarGraph.java
@@ -0,0 +1,40 @@
+package com.group7.mockexpert;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WritingBarGraph extends AppCompatActivity {
+
+    EditText etAnswer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_writing_bar_graph);
+
+        etAnswer = findViewById(R.id.et_answerBarGraph);
+
+    }
+
+    public void btnSubmit(View view) {
+
+        String answer = etAnswer.getText().toString();
+        if (answer.isEmpty()){
+            Toast.makeText(this, "Please write your answer.", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Answer Submitted.", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, WritingTask1.class);
+            startActivity(intent);
+        }
+    }
+}
